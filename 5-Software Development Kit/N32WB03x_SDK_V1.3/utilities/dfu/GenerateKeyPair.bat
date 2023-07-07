1072421201b35ee03c2b96060f34eafbac1add6e
Joe <527757056@qq.com>
2023-07-07 10:01:11 +0800

    remove sdk code form dk

--- 5-Software Development Kit/N32WB03x_SDK_V1.3/utilities/dfu/GenerateKeyPair.bat
+++
@@ -1,6 +0,0 @@
-.\NSUtil\NSUtil.exe keys generate-private-key .\keys\private_key.pem
-.\NSUtil\NSUtil.exe keys generate-public-key .\keys\public_key --private_key .\keys\private_key.pem
-
-PAUSE
-EXIT
-
--- 5-Software Development Kit/N32WB03x_SDK_V1.3/utilities/dfu/JLink/ReadImageUpdate.bat
+++
@@ -1,4 +0,0 @@
-JLink_V632\JLink.exe -Device N32WB031 -If SWD -Speed 4000 -commanderscript JLink_Script\read_imageupdate.jlink
-
-PAUSE
-EXIT
